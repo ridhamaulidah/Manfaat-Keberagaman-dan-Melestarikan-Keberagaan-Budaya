e1433dabf1a77fba42c5a403967c1fdeca3f81c0
ridhamaulidah <ridhamaulidah519@gmail.com>
2025-04-29 14:34:39 +0700

    update

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,18 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6S0LgGtS0lV":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  var audio = document.getElementById("bgSound");
+audio.src ="audio.mp3"
+audio.load();
+audio.play();
+
 }
 
